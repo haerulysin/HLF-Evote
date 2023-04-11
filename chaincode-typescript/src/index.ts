b797761c42913fcaebd93bd89db6185806c7b4b0
Muh Khaerul <m.khaerul15@gmail.com>
2023-04-11 16:28:52 +0700

    fix folder

--- chaincode-typescript/src/index.ts
+++
@@ -1,8 +0,0 @@
-/*
- * SPDX-License-Identifier: 
- */
-
-import { MyAssetContract } from './my-asset-contract';
-export { MyAssetContract } from './my-asset-contract';
-
-export const contracts: any[] = [ MyAssetContract ];
